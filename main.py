e4358ef57f8e8d3fb96c8bdfb150dc0647d15370
Takoprok <edsonyfabian@gmail.com>
2023-03-05 20:19:52 -0300

    Cambios en master! Compaye

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ class Animal:
     def __init__(self, name, action):
         self.name = name
         self.action = action
-
-    def run(self):
-        print(f"{self.name} {self.action}")
+        self.status = 'Alive'
+        
+    def accion(self):
+        print(f"{self.name} is doing this: {self.action}")
 
 perro = Animal("perro", "hace Woof!")
 print("Hallo world desde el main!")
