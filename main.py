b06bfe2a9686dea12c19ff2dcec8e7d80b3e50ec
Takoprok <edsonyfabian@gmail.com>
2023-03-05 20:31:01 -0300

    Mas funcionalidades y constructores

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ class Animal:
     def __init__(self, name, action):
         self.name = name
         self.action = action
-<<<<<<< HEAD
-        self.status = 'Alive'
-        
-    def accion(self):
-        print(f"{self.name} is doing this: {self.action}")
-=======
         self.status = "Alive"
 
+    def change_status(self, new_status):
+        self.status = new_status
+        return self
+    
     def make_action(self):
         print(f"{self.name} {self.action}")
->>>>>>> classtest
-
+        return self
+    
 perro = Animal("perro", "hace Woof!")
-print("Hallo world desde el main!")
+gato = Animal("gato", "hace Miau!")
+print("Hallo, Ciao!")
+
+perro.make_action()
+gato.make_action()
